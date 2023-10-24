5b3b5e090ce8ce2f156c46d7d26c06489f5c0b9a
Achilles <2832468078@qq.com>
2023-10-25 02:41:42 +0800

    完成基础工程的搭建

---
+++ ShineOnce-review/src/test/java/com/axkea/review/ReviewTest.java
@@ -0,0 +1,7 @@
+package com.axkea.review;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ReviewTest {
+}
---
+++ ShineOnce-search/src/test/java/com/axkea/search/SearchTest.java
@@ -0,0 +1,7 @@
+package com.axkea.search;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SearchTest {
+}
